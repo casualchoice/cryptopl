ecaf209e822f995707034a1a2fcaa4756293f0f6
Nitin Sukheja <nitinsukheja86@gmail.com>
2017-12-17 06:07:54 -0500

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,185 @@
+$(document).ready(function() {
+  $.ajax({
+    dataType: "json",
+    url: "data/custom.json?" +new Date().getTime(),
+    data: {},
+    success: function (res) {
+      localStorage.setItem("basicExchangesData", JSON.stringify(res))
+      usdCurrencyData["dollar_rate"] = res.dollar_rate
+      initializeUSDTab()
+    }
+  })
+  document.getElementById("btnreload").addEventListener("click", function(){
+      usdCurrencyData.dollar = Number($('#txtdollar').val())
+      usdCurrencyData.dollar_rate = Number($('#txtdollar_rate').val())
+      initializeUSDTab()
+   });
+})
+
+// Usd functions
+let usdCurrencyData = {
+  "dollar" : 500
+}
+
+let cryptoPrices = {
+  "exchanges" : {}
+}
+
+function initializeUSDTab() {
+  const basicExchangesData = JSON.parse(localStorage.getItem("basicExchangesData"));
+  $.each(basicExchangesData.exchanges, function(key, value) {
+    //if(value.accepts_usd_visa === 1) {
+      var bankFactor = 0.035
+  		var gstFactor = 0.18
+      var usdTotal = usdCurrencyData.dollar * (1 + value.visa_commision/100)  + value.visa_commision_tax
+      var lblBankTrans = round(usdTotal * usdCurrencyData.dollar_rate,2)
+      var lblBankMargin = round(lblBankTrans * basicExchangesData.bank_factor,2)
+      var lblBankGST = round(lblBankMargin * basicExchangesData.gst_factor,2)
+      lblBankTotal = round(lblBankTrans + lblBankMargin  + lblBankGST,2)
+      usdCurrencyData[key+"_usd"] = value.accepts_usd_visa === 1 ? usdTotal : usdCurrencyData.dollar
+      usdCurrencyData[key+"_inr"] = value.accepts_usd_visa === 1 ? lblBankTotal : usdCurrencyData.dollar_rate * usdCurrencyData.dollar
+  //  }
+  })
+  populateDollarValues()
+  loadCryptoPrices()
+}
+
+function loadCryptoPrices() {
+  const basicExchangesData = JSON.parse(localStorage.getItem("basicExchangesData"));
+  let deferredExchangeCalls = []
+  $.each(basicExchangesData.exchanges, function(exchange, basicData) {
+    let callback = $.Deferred();
+    deferredExchangeCalls.push(callback)
+    if (exchange != "bittrex") {
+      $.ajax({url: basicData.api_url,
+        success: function(result) {
+          loadExchangePrice(exchange, basicData, result, callback)
+        }
+      })
+    } else {
+      loadBittrexData(basicData, callback)
+    }
+  })
+  $.when.all(deferredExchangeCalls).then(function ( cryptos ) {
+    $.each(cryptos, function(index, value) {
+       let key = Object.keys(value)[0]
+       cryptoPrices.exchanges[key] = value[key]
+    })
+    populateCryptoPrices()
+    populateBuySellMarkets()
+  });
+}
+
+function loadExchangePrice (exchange, basicData, apiResult, callback) {
+  switch (exchange) {
+    case "cex":
+      loadCexPrices(basicData, apiResult, callback)
+      break;
+    case "koinex":
+      loadKoinexPrices(basicData, apiResult, callback)
+      break;
+    case "bitfinex":
+      loadBitfinexPrices(basicData, apiResult, callback)
+      break;
+    default:
+  }
+}
+
+function loadCexPrices (basicData, apiResult, callback) {
+   if(apiResult.ok === "ok") {
+    let cryptoPrice = {}
+    let retvalue = {}
+    retvalue["cex"] = cryptoPrice
+    $.each(apiResult.data, function(index, crypto) {
+      //if(basicData.crypto_supported.indexOf(crypto.symbol1) != -1 && crypto.symbol2 === "USD") {
+      if(crypto.symbol2 === "USD") {
+        cryptoPrice[crypto.symbol1] = {}
+        cryptoPrice[crypto.symbol1]["lpriceUsd"] = round(Number(crypto.lprice),2)
+        cryptoPrice[crypto.symbol1]["lpriceInr"] = round(Number(crypto.lprice )* usdCurrencyData.dollar_rate,2)
+        cryptoPrice[crypto.symbol1]["crypto"] = round(usdCurrencyData.dollar/Number(crypto.lprice),4)
+      }
+      //}
+    })
+
+    callback.resolve( retvalue)
+   }
+}
+
+function loadKoinexPrices (basicData, apiResult, callback) {
+ let cryptoPrice = {}
+ let retvalue = {}
+ retvalue["koinex"] = cryptoPrice
+ let lprices = apiResult.prices;
+ $.each(apiResult.prices, function(sym, crypto) {
+   cryptoPrice[sym] = {}
+   cryptoPrice[sym]["lpriceUsd"] = round(crypto / usdCurrencyData.dollar_rate,2)
+   cryptoPrice[sym]["lpriceInr"] = Number(crypto)
+   cryptoPrice[sym]["crypto"] = round(usdCurrencyData["koinex_inr"] / crypto,4)
+ })
+  callback.resolve(retvalue)
+}
+
+function loadBitfinexPrices(basicData, apiResult, callback) {
+  let cryptoPrice = {}
+  let retvalue = {}
+  retvalue["bitfinex"] = cryptoPrice
+  //let lprices = apiResult.prices;
+   for (var index in apiResult) {
+    const symbol = apiResult[index][0].substring(1,4)
+    cryptoPrice[symbol] = {}
+    cryptoPrice[symbol]["lpriceUsd"] = round(apiResult[index][1],2)
+    cryptoPrice[symbol]["lpriceInr"] = round(Number(usdCurrencyData.dollar_rate * apiResult[index][1]),2)
+    cryptoPrice[symbol]["crypto"] = round(usdCurrencyData["bitfinex_usd"] / apiResult[index][1],4)
+  }
+   callback.resolve(retvalue)
+}
+
+function loadBittrexData(basicData, callback) {
+  let deferredCryptoCalls = []
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const basicExchangesData = JSON.parse(localStorage.getItem("basicExchangesData"));
+  let retvalue = {}
+  let cryptoPrice ={}
+  retvalue["bittrex"] = cryptoPrice
+  $.each(basicExchangesData.global_crypto_supported, function(index, symbol) {
+    let callbackCrypto = $.Deferred();
+    deferredCryptoCalls.push(callbackCrypto)
+    $.ajax({
+      url: proxyurl + basicData.api_url + symbol.replace("BCH","BCC"),
+      success: function(response){
+        if(response.success){
+          let lprice = response.result[0].Last
+          cryptoPrice[symbol] = {}
+          cryptoPrice[symbol]["lpriceUsd"] = round(lprice,2)
+          cryptoPrice[symbol]["lpriceInr"] = round(Number(usdCurrencyData.dollar_rate * lprice),2)
+          cryptoPrice[symbol]["crypto"] = round(usdCurrencyData["bitfinex_usd"] / lprice,4)
+          callbackCrypto.resolve(cryptoPrice)
+        }
+      }
+    })
+  })
+  $.when.all(deferredCryptoCalls).then(function ( cryptos ) {
+    callback.resolve(retvalue)
+  });
+
+}
+
+function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+
+
+// Put somewhere in your scripting environment
+if (typeof jQuery.when.all === 'undefined') {
+  jQuery.when.all = function (deferreds) {
+    return $.Deferred(function (def) {
+      $.when.apply(jQuery, deferreds).then(
+          function () {
+              def.resolveWith(this, [Array.prototype.slice.call(arguments)]);
+          },
+          function () {
+              def.rejectWith(this, [Array.prototype.slice.call(arguments)]);
+          });
+    });
+  }
+}
---
+++ js/html.js
@@ -0,0 +1,83 @@
+
+function populateDollarValues() {
+  //let bstColoumNo = Object.keys(usdCurrencyData).length/12
+  $('#usdHeader').html('')
+  $('#usdConversions').html('')
+  $.each(usdCurrencyData, function(key, value) {
+    $('#usdHeader').append($("<div class='col-sm-1'></div>").text(key));
+    if(key.indexOf("dollar") != -1) {
+      $('#usdConversions').append($("<div class='col-sm-1 warning'></div>")
+      .append('<input type="text" value="' +value+ '" style="color: black; width:50px" id="txt' + key+ '"></input>'));
+    } else {
+      $('#usdConversions').append($("<div class='col-sm-1'></div>").text(value));
+    }
+  });
+}
+
+function populateCryptoPrices () {
+  $('#cryptoPricesHeader').html('')
+  $('#cryptoPrices').html('')
+  $('#cryptoPricesHeader').append($("<th></th>").text("Exchange"));
+  const basicExchangesData = JSON.parse(localStorage.getItem("basicExchangesData"));
+  $.each(basicExchangesData.global_crypto_supported, function(index, symbol) {
+      $('#cryptoPricesHeader').append($("<th></th>").text(symbol));
+  })
+  $.each( cryptoPrices.exchanges, function(exchangeKey, exchange) {
+     let row = "<tr><td>" + exchangeKey + "</td>"
+
+    $.each(basicExchangesData.global_crypto_supported, function(index, symbol) {
+      let cryptoColumn  = exchange[symbol].lpriceInr + " (" + exchange[symbol].lpriceUsd + ") - " + exchange[symbol].crypto
+      row += "<td >" + cryptoColumn + "</td>"
+    })
+    $('#cryptoPrices').append(row +"</tr>")
+  })
+}
+
+function populateBuySellMarkets() {
+  $('#buySellMarkets').html('')
+  const basicExchangesData = JSON.parse(localStorage.getItem("basicExchangesData"));
+  let html = ""
+  $.each(basicExchangesData.buy_sell_markets, function(index, market) {
+    if (index % 2 == 0 ) {
+      html =  "<div class='row'>"
+    }
+    html += "<div class='col-sm-6'><h3>"+market+"</h3>"+ getBuySellMarket(market)+"</div>"
+    if ((index+1) % 2 == 0 || index+1 == basicExchangesData.buy_sell_markets.length) {
+      html +=  "</div>"
+      $('#buySellMarkets').append(html)
+    }
+  })
+}
+
+function getBuySellMarket(market) {
+  const basicExchangesData = JSON.parse(localStorage.getItem("basicExchangesData"));
+
+  let table = "<table class='table table-striped'>" +
+                "<thead>" +
+                  "<tr class='warning'>" +
+                    "<th>Coin</th>" +
+                    "<th>Diff</th>" +
+                    "<th>Crypto</th>" +
+                    "<th>Amount</th>" +
+                    "<th>PL</th>" +
+                  "</tr>" +
+                "</thead>" +
+                "<tbody >"
+  let buyExchange = cryptoPrices.exchanges[market.split("-")[0]]
+  let sellExchange = cryptoPrices.exchanges[market.split("-")[1]]
+  $.each(basicExchangesData.global_crypto_supported, function(index, symbol) {
+      let buyPrice = buyExchange[symbol].lpriceInr
+      let sellPrice = sellExchange[symbol].lpriceInr
+      let crypto = buyExchange[symbol].crypto - basicExchangesData.exchanges[market.split("-")[0]].withdrawal_charges[symbol]
+      let inr = round(crypto *  sellExchange[symbol].lpriceInr,2)
+      table += "<tr><td>" + symbol + "</td>"
+      table += "<td>" + round (100 * ((sellPrice - buyPrice) / buyPrice) ,2)+ "</td>"
+      table += "<td>" + round(crypto,4) + "</td>"
+      table += "<td>" + inr + "</td>"
+      table += "<td>" + round((inr - usdCurrencyData[market.split("-")[0]+"_inr"]),2) + "</td>"
+      table += "</tr>"
+  })
+  table += "</tbody></table>"
+
+  return table
+}
